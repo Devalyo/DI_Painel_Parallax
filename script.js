21029cd96ef05312adb1d8cc8074c9e6d95b9647
Daniel de Carvalho <117108120+Devalyo@users.noreply.github.com>
2024-11-04 18:45:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -18,25 +18,24 @@ function switchCards() {
   
   if (isGhibli) {
     document.body.style.background = "linear-gradient(to bottom, #ac843e, #5c4735)";
-
     img1.src="./assets/gogh.png"
     img1.style.height = "110%";
     img1.style.top = "14px"
     img1.style.right = "30px"
-    backgrounds.item(0).style.background = "url('./assets/background1.png')"
+    backgrounds.item(0).style.background = "url('./assets/background1.png') center / cover no-repeat"
     backgrounds.item(0).style.filter = "blur(2px)";
     
     img2.src="./assets/circe.png"
     img2.style.scale = "120%"
     img2.style.top = "3vh";
-    backgrounds.item(1).style.background = "url('./assets/background2.png')"
+    backgrounds.item(1).style.background = "url('./assets/background2.png') center / cover no-repeat"
     backgrounds.item(1).style.filter = "brightness(120%) blur(2px)";
     
     img3.src="./assets/borea.png"
     img3.style.top = "5px;"
     img3.style.left = "-1vw;"
     img3.style.height = "110%"
-    backgrounds.item(2).style.background = "url('./assets/background3.png')"
+    backgrounds.item(2).style.background = "url('./assets/background3.png') center / cover no-repeat"
     backgrounds.item(2).style.filter = "blur(0px) brightness(70%)";
     
   
@@ -53,7 +52,7 @@ function switchCards() {
   img1.style.height = "110%";
   img1.style.top = "14px"
   img1.style.right = "-10px"
-  backgrounds.item(0).style.background = "url('./assets/background_g1.jpg"
+  backgrounds.item(0).style.background = "url('./assets/background_g1.jpg') center / cover no-repeat"
   backgrounds.item(0).style.filter = "blur(0px)";
   
   
@@ -61,7 +60,7 @@ function switchCards() {
   img2.src= "./assets/3dr_chihiro.png"
   img2.style.scale = "100%"
   img2.style.top = "25px";
-  backgrounds.item(1).style.background = "url('./assets/background_g2.jpg"
+  backgrounds.item(1).style.background = "url('./assets/background_g2.jpg') center / cover no-repeat"
   backgrounds.item(1).style.filter = "blur(0px)";
 
   img3.src="./assets/3dr_howlcastle.png"
@@ -69,7 +68,7 @@ function switchCards() {
   img3.style.top = "5px"
   img3.style.right = "0"
   img3.style.left = "-4px"
-  backgrounds.item(2).style.background = "url('./assets/background_g3.jpg"
+  backgrounds.item(2).style.background = "url('./assets/background_g3.jpg') center / cover no-repeat"
   backgrounds.item(2).style.filter = "blur(0px) brightness(100%);";
   
   var palavra = document.querySelector(".textoCards")
